0a6b01111abca33b5371b0e1105e5b9155cc3dce
CornelioM <corneliomanzueta169@gmail.com>
2022-07-06 17:23:33 -0400

    agregando practicas javascript

---
+++ main.js
@@ -0,0 +1,28 @@
+/*
+function estoEsUnaFuncion(){
+console.log("Esto es una funcion mensaje uno");
+console.log("Esto es una funcion mensaje dos");
+console.log("Esto es una funcion mensaje tres");
+console.log("Esto es una funcion mensaje cuatro");
+}
+function FuncionQueDevuelveUnaCadenaDeTexto(){
+    console.log("Esto es una funcion devuelve mensaje uno");
+    console.log("Esto es una funcion devuelve mensaje dos");
+    console.log("Esto es una funcion devuelve mensaje tres");
+    console.log("Esto es una funcion devuelve mensaje cuatro");
+    return "Yo soy Cornelio Manzueta un Programador Web";
+}
+estoEsUnaFuncion();
+FuncionQueDevuelveUnaCadenaDeTexto();
+Saludar("Cornelio",29);
+Saludar2(,29);
+let valorDeFuncion = FuncionQueDevuelveUnaCadenaDeTexto();
+console.log(valorDeFuncion);
+
+function Saludar(nombre, edad ){
+console.log(`Hola mi nombre es ${nombre} y tengo ${edad} anos`);
+}
+function Saludar2(nombre = "Desconocido",edad = 0){
+    console.log(`Hola mi nombre es ${nombre} y tengo ${edad} anos`);
+}
+*/
